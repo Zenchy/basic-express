1575c4dd2479dabe8a3e51668e33b30cb839f4f5
Alvaro Corral <alvaro.corral@innocv.com>
2020-04-26 20:17:33 +0200

    Routing basic and routing methods

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
+app.use('/user', (req,res, next) => {
+  console.log('Send /user >>>>> OK')
+  next()
+})
+
 app.get('/', (req,res) => {
   res.send('<h1>Hi we got the response!!!</h1>')
 })
 
-app.get('/about', (req, res) => {
-  res.send('This is the app basic developing')
+app.get('/user', (req, res) => {
+  res.json({
+    username: 'Zenchy',
+    lastname: 'poderoso'
+  })
+})
+
+app.post('/user/:id', (req, res) => {
+  console.log(req.body)
+  console.log(req.params)
+  res.send('This is the POST')
+})
+
+app.delete('/user/:userId', (req, res) => {
+  res.send(`User ${req.params.userId} deleted`)
+})
+
+app.put('/user/:id', (req, res) => {
+  console.log(req.body)
+  res.send(`User ${req.params.id} updated`)
+})
+
+app.get('/get', (req, res) => {
+  res.send('This is the GET')
+})
+
+app.post('/post', (req, res) => {
+  res.send('This is the POST')
+})
+
+app.put('/put', (req, res) => {
+  res.send('This is the PUT')
+})
+
+app.delete('/delete', (req, res) => {
+  res.send('This is the DELETE')
 })
 
 app.get('/contact', (req, res) => {
