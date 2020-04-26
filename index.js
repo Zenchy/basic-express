6c19e920cff7ee8b5754224372856664a8c63e93
Alvaro Corral <alvaro.corral@innocv.com>
2020-04-26 06:26:20 +0200

    Nodmon and scripts with basic routing

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.get('/', (req,res) => {
   res.send('<h1>Hi we got the response!!!</h1>')
 })
 
+app.get('/about', (req, res) => {
+  res.send('This is the app basic developing')
+})
+
+app.get('/contact', (req, res) => {
+  res.send('This is my contact in the net')
+})
+
 app.listen(3000, () => {
-  console.log('Serever on port 3000 with express!!')
+  console.log('Serever on port 3000 with express now with nodemon too!!')
 })
